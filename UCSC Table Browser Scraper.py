82d8dab07de25412bbe5a418d3e3e3bf69f8b629
GeoffSCollins <39243170+GeoffSCollins@users.noreply.github.com>
2018-12-16 20:42:55 -0600

    pep8 changes

--- UCSC Table Browser Scraper.py
+++ UCSC Table Browser Scraper.py
@@ -4,8 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
+from tkinter import ttk, messagebox
 import datetime
 import sys
 
@@ -23,7 +22,8 @@ class TableBrowserStats(tk.Tk):
         self.inputBoxURL = tk.Text(self.master, height=1, width=50)
         self.inputBoxURL.grid(row=2)
 
-        self.label = tk.Label(self.master, text="Please enter the regions you want the mean, min and max from:").grid(row=3)
+        self.label = tk.Label(self.master, text="Please enter the regions you want the mean,"
+                                                " min and max from:").grid(row=3)
 
         self.inputBox = tk.Text(self.master, height=25, width=50)
         self.inputBox.grid(row=4)
@@ -32,72 +32,70 @@ class TableBrowserStats(tk.Tk):
 
     # Getting the inputted data from the user after pressing the start button and running the program
     def start(self):
-        tableBrowserURL = str(self.inputBoxURL.get("1.0", tk.END))
-        tableBrowserURL = tableBrowserURL.rstrip()
+        table_browser_url = str(self.inputBoxURL.get("1.0", tk.END)).rstrip()
+        regions_box_text = self.inputBox.get("1.0", tk.END).rstrip()
 
-        regionsBoxText = self.inputBox.get("1.0", tk.END).rstrip()
-        if regionsBoxText:
+        if regions_box_text:
             with open('Gene File From Script.txt', 'w') as geneFile:
                 geneFile.write(self.inputBox.get("1.0", tk.END))
             self.master.destroy()
-            self.makeProgressBar()
+            self.make_progress_bar()
 
             try:
                 self.driver = webdriver.Chrome(os.getcwd() + "/chromedriver.exe")
-                self.getStatistics(self, tableBrowserURL)
+                self.get_statistics(self, table_browser_url)
             except Exception as e:
                 self.driver.quit()
                 self.progressBar.close()
-                restartBox(e)
+                RestartBox(e)
 
         else:
-            restartBox("The regions box was empty.")
+            RestartBox("The regions box was empty.")
             self.master.destroy()
 
-    def makeProgressBar(self):
+    def make_progress_bar(self):
         self.progressBar = ProgressBar()
-        self.progressBar.updateProgressBar(0)
 
     # Main program that actually retrieves data from UCSC Table Browser
     @staticmethod
-    def getStatistics(self, URL):
-        self.driver.get(URL)
+    def get_statistics(self, url):
+        self.driver.get(url)
 
         # This gets the names of the tracks
         select_box = self.driver.find_element_by_name("hgta_track")
-        optionsArr = [x for x in select_box.find_elements_by_tag_name("option")]
-        nameArr = [0] * len(optionsArr)
+        options_array = [x for x in select_box.find_elements_by_tag_name("option")]
+        name_array = [0] * len(options_array)
 
-        for i in range(len(optionsArr)):
-            element = optionsArr[i]
-            nameArr[i] = element.get_attribute("value")
+        for i in range(len(options_array)):
+            element = options_array[i]
+            name_array[i] = element.get_attribute("value")
 
         # Get lines that have regions in them
-        numLines = sum(1 for line in open('Gene File From Script.txt') if not len(line.strip()) == 0)
+        num_lines = sum(1 for line in open('Gene File From Script.txt') if not len(line.strip()) == 0)
 
         with open('Gene File From Script.txt', 'r') as geneFile:
             for lineNum, line in enumerate(geneFile):
-                geneName = line.split()[0]
+                gene_name = line.split()[0]
                 region = line.split()[1]
 
-                regionElement = self.driver.find_element_by_name("position")
-                regionElement.clear()
-                regionElement.send_keys(region)
+                region_element = self.driver.find_element_by_name("position")
+                region_element.clear()
+                region_element.send_keys(region)
 
                 select = Select(self.driver.find_element_by_id("hgta_track"))
                 options = select.options
 
                 dir_path = os.path.dirname(os.path.realpath(__file__))
-                fileName = dir_path + '/Output/' + geneName + '.txt'
+                file_name = dir_path + '/Output/' + gene_name + '.txt'
 
-                with open(fileName, 'w') as outputFile:
+                with open(file_name, 'w') as outputFile:
                     outputFile.write("Gene Name\tTrack Name\tRegion\tMean\tMin\tMax\n")
 
                     for index in range(len(options)):
                         if index == 0:
-                            self.progressBar.setFinishTime(int(numLines * len(options)))
+                            self.progressBar.set_finish_time(int(num_lines * len(options)))
 
-                        trackName = nameArr[index]
+                        track_name = name_array[index]
                         select = Select(self.driver.find_element_by_id("hgta_track"))
                         options = select.options
 
@@ -108,32 +106,32 @@ class TableBrowserStats(tk.Tk):
 
                         soup = BeautifulSoup(file, 'html.parser')
 
-                        goodText = soup.text[soup.text.find('mean'):soup.text.find('standard deviation')].rstrip()
+                        ucsc_output_table = soup.text[soup.text.find('mean'):soup.text.find('standard deviation')].rstrip()
 
-                        meanLine = goodText.partition('\n')[0]
-                        startPosFirst = meanLine.find('n')+1
-                        mean = float(meanLine[startPosFirst:])
+                        mean_line = ucsc_output_table.partition('\n')[0]
+                        mean_line_start_position = mean_line.find('n')+1
+                        mean = float(mean_line[mean_line_start_position:])
 
-                        minLine = goodText.split('\n')[1]
-                        startPosSec = minLine.find('n') + 1
-                        min = float(minLine[startPosSec:])
+                        min_line = ucsc_output_table.split('\n')[1]
+                        min_line_start_position = min_line.find('n') + 1
+                        ucsc_min = float(min_line[min_line_start_position:])
 
-                        startThirdLine = goodText.find("max")
-                        maxLine = goodText[startThirdLine:]
-                        startPosThird = maxLine.find('x') + 1
-                        max = float(maxLine[startPosThird:])
+                        max_line = ucsc_output_table[ucsc_output_table.find("max"):]
+                        max_line_start_position = max_line.find('x') + 1
+                        ucsc_max = float(max_line[max_line_start_position:])
 
                         # Print data to another file
-                        outputFile.write(str(geneName) + '\t' + str(trackName) + '\t'+ str(region) + '\t' + str(mean) + '\t' + str(min) + '\t' + str(max) + '\n')
+                        outputFile.write(str(gene_name) + '\t' + str(track_name) + '\t'+ str(region) + '\t' + str(mean)
+                                         + '\t' + str(ucsc_min) + '\t' + str(ucsc_max) + '\n')
 
                         time.sleep(10)
                         self.driver.execute_script("window.history.go(-1)")
-                        percentComplete = (index + 1) / len(options)
-                        self.progressBar.updateProgressBar(percentComplete/numLines)
+                        percent_complete = (index + 1) / len(options)
+                        self.progressBar.update_progress_bar(percent_complete/num_lines)
                         time.sleep(5)
-
         self.driver.quit()
         sys.exit(0)
+
 
 class ProgressBar(tk.Tk):
     def __init__(self):
@@ -142,27 +140,26 @@ class ProgressBar(tk.Tk):
         self.popup.title("Progress")
 
         self.progress = ttk.Progressbar(self.popup, orient="horizontal", length=200, mode="determinate")
-
         self.value = 0
         self.progress["value"] = 0
         self.max = 1
         self.progress["maximum"] = 1
 
         self.startTime = datetime.datetime.now()
-
         self.startText = tk.Label(self.popup, text="Start time: " + str(self.startTime.time().replace(microsecond=0)))
         self.finishText = tk.Label(self.popup, text="Finish time: Plese wait...")
-
         self.progress.pack()
         self.startText.pack()
         self.finishText.pack()
 
-    def updateProgressBar(self, value):
+        self.update_progress_bar(0)
+
+    def update_progress_bar(self, value):
         self.value = value
         self.progress["value"] = self.value
         tk.Tk.update(self.popup)
 
-    def setFinishTime(self, iterations):
+    def set_finish_time(self, iterations):
         self.finishTime = self.startTime + datetime.timedelta(seconds=(iterations * 15)+10)
         self.finishText.config(text="Finish time: " + str(self.finishTime.time().replace(microsecond=0)))
         self.finishText.pack()
@@ -171,7 +168,8 @@ class ProgressBar(tk.Tk):
     def close(self):
         self.popup.destroy()
 
-class restartBox(tk.Tk):
+
+class RestartBox(tk.Tk):
 
     def __init__(self, error):
         self.result = messagebox.askyesno("Do you want to restart?", "This error occured: " + str(error) + "\nDo you want to restart?", icon='warning')
@@ -188,5 +186,5 @@ if __name__ == "__main__":
 
     root = tk.Tk()
     root.withdraw()
-    GUI = TableBrowserStats()
+    TableBrowserStats()
     root.mainloop()
